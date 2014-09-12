36ec87dcac7b7501a07911c2d1e36c917e89da34
HeroicKatora <andreas.molzer@gmx.de>
2014-09-12 21:12:03 +0200

    Two interfaces to make questing area implementation possible.

---
+++ src/main/java/mhfc/net/common/quests/world/IQuestArea.java
@@ -0,0 +1,13 @@
+package mhfc.net.common.quests.world;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+public interface IQuestArea {
+
+	public IQuestAreaSpawnController getSpawnController();
+
+	public void teleportToSpawn(EntityPlayer player);
+
+	public String getUnlocalizedDisplayName();
+
+}
