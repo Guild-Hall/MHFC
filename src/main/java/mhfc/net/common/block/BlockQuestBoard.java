07d97f42b9b4e49653fc307976ba7c06a9db0ac9
HeroicKatora <andreas.molzer@gmx.de>
2015-02-27 19:58:23 +0100

    I don't even know if that is really patch

--- src/main/java/mhfc/net/common/block/BlockQuestBoard.java
+++ src/main/java/mhfc/net/common/block/BlockQuestBoard.java
@@ -21,8 +21,6 @@ import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockQuestBoard extends BlockContainer {
 
@@ -79,13 +77,6 @@ public class BlockQuestBoard extends BlockContainer {
 	@Override
 	public TileEntity createNewTileEntity(World var1, int var2) {
 		return new TileQuestBoard();
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public Item getItem(World p_149694_1_, int p_149694_2_, int p_149694_3_,
-			int p_149694_4_) {
-		return null;
 	}
 
 	@Override
