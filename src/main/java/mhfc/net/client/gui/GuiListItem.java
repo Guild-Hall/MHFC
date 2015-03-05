e4a265ea5fd462d7b90dcf1a3e05357491f2084a
HeroicKatora <andreas.molzer@gmx.de>
2015-03-05 12:31:41 +0100

    Fixed a small bug in list item rendering

--- src/main/java/mhfc/net/client/gui/GuiListItem.java
+++ src/main/java/mhfc/net/client/gui/GuiListItem.java
@@ -1,5 +1,6 @@
 package mhfc.net.client.gui;
 
+import java.util.Iterator;
 import java.util.List;
 
 import net.minecraft.client.Minecraft;
@@ -17,17 +18,22 @@ public abstract class GuiListItem {
 
 	public void draw(int x, int y, int width, int height, Minecraft m,
 			boolean selected, Alignment alignment) {
+		int innerStringWidth = width - 5;
 		FontRenderer fRend = m.fontRenderer;
-		String representation = getRepresentationString();
+		String representation = getRepresentationString().trim();
 		@SuppressWarnings("unchecked")
 		List<String> lineWrapped = fRend.listFormattedStringToWidth(
-				representation, width);
-		int lines = lineWrapped.size();
+				representation, innerStringWidth);
 		int stringLength = 0;
-		for (String s : lineWrapped) {
-			stringLength = Math.max(stringLength, fRend.getStringWidth(s));
+		Iterator<String> it = lineWrapped.iterator();
+		while (it.hasNext()) {
+			String s = it.next();
+			int thisStringLength = fRend.getStringWidth(s);
+			stringLength = Math.max(stringLength, thisStringLength);
 		}
-		int color = (selected) ? 0x404040 : 0x808080;
+		int lines = lineWrapped.size();
+
+		int positionY = y + (height - (fRend.FONT_HEIGHT) * lines) / 2 + 1;
 		int positionX = x;
 		switch (alignment) {
 			case LEFT :
@@ -37,10 +43,11 @@ public abstract class GuiListItem {
 				positionX += (width - stringLength) / 2;
 				break;
 			case RIGHT :
-				positionX += stringLength - 3;
+				positionX += width - stringLength - 3;
 		}
-		fRend.drawSplitString(representation, positionX, y
-				+ (height - (fRend.FONT_HEIGHT) * lines) / 2 + 1, width - 5,
-				color);
+
+		int color = (selected) ? 0x404040 : 0x808080;
+		fRend.drawSplitString(representation, positionX, positionY,
+				innerStringWidth, color);
 	}
 }
