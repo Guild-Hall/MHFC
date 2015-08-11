06521efe61804e7148fdf37305918f8d168a6de5
Heltrato <heltrato@gmail.com>
2015-08-11 17:10:22 +0800

    -Help can smeone implement AIGeneralIdle?

--- src/main/java/mhfc/net/common/ai/general/actions/AIGeneralIdle.java
+++ src/main/java/mhfc/net/common/ai/general/actions/AIGeneralIdle.java
@@ -1,5 +1,22 @@
 package mhfc.net.common.ai.general.actions;
 
-public class AIGeneralIdle {
+import mhfc.net.common.ai.ActionAdapter;
+import mhfc.net.common.entity.type.EntityMHFCBase;
+
+public class AIGeneralIdle <EntityT extends EntityMHFCBase<? super EntityT>>
+extends
+ActionAdapter<EntityT> {
+	{
+}
+
+	@Override
+	protected void update() {
+		
+	}
+
+	@Override
+	public float getWeight() {
+		return 0;
+	}
 
 }
