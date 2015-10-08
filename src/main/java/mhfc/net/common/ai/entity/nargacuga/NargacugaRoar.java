cc2f3274a55933fd0f7750fa888e8020ae17e9fe
HeroicKatora <andreas.molzer@gmx.de>
2015-10-08 22:05:15 +0200

    Adding files again that were removed to fix conflicts that were just created by moving the files and which I then forgot to add again.

---
+++ src/main/java/mhfc/net/common/ai/entity/nargacuga/NargacugaRoar.java
@@ -0,0 +1,28 @@
+package mhfc.net.common.ai.entity.nargacuga;
+
+import mhfc.net.common.ai.general.actions.AIGeneralRoar;
+import mhfc.net.common.ai.general.provider.IAnimationProvider;
+import mhfc.net.common.ai.general.provider.ISelectionPredicate;
+import mhfc.net.common.ai.general.provider.IWeightProvider;
+import mhfc.net.common.entity.mob.EntityNargacuga;
+
+public class NargacugaRoar extends AIGeneralRoar<EntityNargacuga> {
+	private static final int LAST_FRAME = 71;
+
+	public NargacugaRoar() {
+		super(generateProvider());
+	}
+
+	private static IRoarProvider<EntityNargacuga> generateProvider() {
+		IAnimationProvider anim = new IAnimationProvider.AnimationAdapter(
+			"mhfc:models/Nargacuga/Roar.mcanm", LAST_FRAME);
+		ISelectionPredicate<EntityNargacuga> select = new ISelectionPredicate.SelectAlways<EntityNargacuga>();
+		IWeightProvider<EntityNargacuga> weight = new IWeightProvider.RandomWeightAdapter<>(
+			1F);
+		IRoarSoundProvider roar = new IRoarSoundProvider.RoarSoundAdapter(
+			"mhfc:nargacuga-roar");
+		IRoarProvider<EntityNargacuga> provide = new AIGeneralRoar.RoarAdapter<>(
+			anim, select, weight, roar);
+		return provide;
+	}
+}
