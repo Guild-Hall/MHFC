5c71526cab35a1edd06758fc2b01b0f4721a4d29
WorldSEnder <worldsbegin@gmx.de>
2015-12-02 23:55:39 +0100

    Server now starts without throwing exceptions
    Who thought it would be cool to have Client-only JsonUtils? probably Jeb

--- src/main/java/mhfc/net/common/quests/factory/HuntingGoalFactory.java
+++ src/main/java/mhfc/net/common/quests/factory/HuntingGoalFactory.java
@@ -1,17 +1,18 @@
 package mhfc.net.common.quests.factory;
 
+import static mhfc.net.common.quests.descriptions.HuntingGoalDescription.ID_AMOUNT;
+import static mhfc.net.common.quests.descriptions.HuntingGoalDescription.ID_HUNTED_TYPE;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
 import mhfc.net.common.quests.api.GoalDescription;
 import mhfc.net.common.quests.api.IGoalFactory;
 import mhfc.net.common.quests.api.QuestGoal;
 import mhfc.net.common.quests.descriptions.HuntingGoalDescription;
 import net.minecraft.entity.EntityList;
 import net.minecraft.util.JsonUtils;
-
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-
-import static mhfc.net.common.quests.descriptions.HuntingGoalDescription.*;
 
 public class HuntingGoalFactory implements IGoalFactory {
 
@@ -21,24 +22,17 @@ public class HuntingGoalFactory implements IGoalFactory {
 	}
 
 	@Override
-	public GoalDescription buildGoalDescription(JsonObject json,
-		JsonDeserializationContext context) {
+	public GoalDescription buildGoalDescription(JsonObject json, JsonDeserializationContext context) {
 		if (!json.has(ID_HUNTED_TYPE) || !json.has(ID_AMOUNT))
-			throw new JsonParseException("A hunting goal needs a "
-				+ ID_HUNTED_TYPE + " and a " + ID_AMOUNT + "attribute");
-		if (!JsonUtils.jsonObjectFieldTypeIsString(json, ID_HUNTED_TYPE))
-			throw new JsonParseException("The target must be a mob id");
-		String mobID = JsonUtils.getJsonElementStringValue(json
-			.get(ID_HUNTED_TYPE), ID_HUNTED_TYPE);
-		Class<?> goalClass = (Class<?>) EntityList.stringToClassMapping
-			.get(mobID);
+			throw new JsonParseException(
+					"A hunting goal needs a " + ID_HUNTED_TYPE + " and a " + ID_AMOUNT + "attribute");
+		String mobID = JsonUtils.getJsonElementStringValue(json.get(ID_HUNTED_TYPE), ID_HUNTED_TYPE);
+		Class<?> goalClass = (Class<?>) EntityList.stringToClassMapping.get(mobID);
 		if (goalClass == null) {
-			throw new JsonParseException("The mob identifier " + mobID
-				+ " could not be resolved");
+			throw new JsonParseException("The mob identifier " + mobID + " could not be resolved");
 		}
 		if (!json.get(ID_AMOUNT).isJsonPrimitive())
-			throw new JsonParseException(
-				"The amount given is not of type integer");
+			throw new JsonParseException("The amount given is not of type integer");
 		int amount = json.get(ID_AMOUNT).getAsInt();
 		return new HuntingGoalDescription(goalClass, amount);
 	}
