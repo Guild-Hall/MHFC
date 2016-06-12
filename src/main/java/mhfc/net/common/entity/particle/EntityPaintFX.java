af93ac81e855f63bea660fa5123f5c044d42dd5c
WorldSEnder <worldsbegin@gmx.de>
2016-06-12 14:54:16 +0200

    float[] -> Color

--- src/main/java/mhfc/net/common/entity/particle/EntityPaintFX.java
+++ src/main/java/mhfc/net/common/entity/particle/EntityPaintFX.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.registry.IEntityAdditionalSpawnData;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import io.netty.buffer.ByteBuf;
+import javafx.scene.paint.Color;
 import mhfc.net.common.item.ItemColor;
 import net.minecraft.client.particle.EntityFX;
 import net.minecraft.world.World;
@@ -17,19 +18,23 @@ public class EntityPaintFX extends EntityFX implements IEntityAdditionalSpawnDat
 	private ItemColor color;
 
 	public EntityPaintFX(World worldIn, ItemColor color, double posXIn, double posYIn, double posZIn) {
-		super(worldIn, posXIn, posYIn, posZIn,
+		super(
+				worldIn,
+				posXIn,
+				posYIn,
+				posZIn,
 				worldIn.rand.nextFloat() * 0.25F,
 				-1 * Math.abs(2.5F * worldIn.rand.nextFloat()),
 				worldIn.rand.nextFloat() * 0.25F);
-		setParticleTextureIndex((int)Math.floor(worldIn.rand.nextDouble() * (texMax - texMin)) + texMin);
-		this.particleMaxAge = (int)(1.5F * (8.0F / (worldIn.rand.nextFloat() * 1.8F + 0.2F)));
+		setParticleTextureIndex((int) Math.floor(worldIn.rand.nextDouble() * (texMax - texMin)) + texMin);
+		this.particleMaxAge = (int) (1.5F * (8.0F / (worldIn.rand.nextFloat() * 1.8F + 0.2F)));
 		this.particleScale = 2.0F;
 		this.color = color;
 
-		float[] floatColor = color.getFloats();
+		Color floatColor = color.getColor();
 		//Note: This says set RBG color, but the order is actually RGB.
 		//Typo on MC's end.
-		setRBGColorF(floatColor[0], floatColor[1], floatColor[2]);
+		setRBGColorF((float) floatColor.getRed(), (float) floatColor.getGreen(), (float) floatColor.getBlue());
 		setAlphaF(0.8F);
 
 	}
--- src/main/java/mhfc/net/common/item/ItemColor.java
+++ src/main/java/mhfc/net/common/item/ItemColor.java
@@ -1,39 +1,36 @@
 package mhfc.net.common.item;
 
-import org.apache.logging.log4j.Level;
-
-import cpw.mods.fml.common.FMLLog;
+import javafx.scene.paint.Color;
 import mhfc.net.MHFCMain;
-import mhfc.net.common.util.lib.MHFCReference;
-import net.minecraft.item.ItemDye;
 
 /**
- * Enum defining all item colors, as well as their custom color
- * in the Monster Hunter palette. May move the palette
+ * Enum defining all item colors, as well as their custom color in the Monster Hunter palette. May move the palette
  * definition to a config file later...
+ *
  * @author Landon
  *
  */
 public enum ItemColor {
-	BLACK ( 0, 0x303030),
-	RED   ( 1, 0xF85858),
-	GREEN ( 2, 0x54843B),
-	BROWN ( 3, 0xBD9729),
-	BLUE  ( 4, 0x5870A0),
-	PURPLE( 5, 0xB890C0),
-	CYAN  ( 6, 0x31A0A0),
-	SILVER( 7, 0x686868),
-	GRAY  ( 8, 0xA0A0A0),
-	PINK  ( 9, 0xE890A0),
-	LIME  (10, 0x80B050),
+	BLACK(0, 0x303030),
+	RED(1, 0xF85858),
+	GREEN(2, 0x54843B),
+	BROWN(3, 0xBD9729),
+	BLUE(4, 0x5870A0),
+	PURPLE(5, 0xB890C0),
+	CYAN(6, 0x31A0A0),
+	SILVER(7, 0x686868),
+	GRAY(8, 0xA0A0A0),
+	PINK(9, 0xE890A0),
+	LIME(10, 0x80B050),
 	YELLOW(11, 0xF8D058),
 	LIBLUE(12, 0x9BE1F5),
 	MAGNTA(13, 0xF931F7),
 	ORANGE(14, 0xF89858),
-	WHITE (15, 0xFFFFFF);
-	private static final String[] colorNames = ItemDye.field_150921_b;
+	WHITE(15, 0xFFFFFF);
+
 	private final int metadata;
 	private final int color;
+
 	private ItemColor(int metadata, int color) {
 		this.metadata = metadata;
 		this.color = color;
@@ -41,6 +38,7 @@ public enum ItemColor {
 
 	/**
 	 * Returns associated metadata.
+	 *
 	 * @return the metadata
 	 */
 	public int getMetadata() {
@@ -48,8 +46,8 @@ public enum ItemColor {
 	}
 
 	/**
-	 * Returns the color as an integer ranging from
-	 * 0x000000 (0) to 0xFFFFFF (16,777,215)
+	 * Returns the color as an integer ranging from 0x000000 (0) to 0xFFFFFF (16,777,215)
+	 *
 	 * @return the color
 	 */
 	public int getRGB() {
@@ -57,38 +55,30 @@ public enum ItemColor {
 	}
 
 	/**
-	 * Returns the color associated with metadata.
-	 * Note that the metadata can be of ANY length;
-	 * it only looks at the last byte.
+	 * Returns the color associated with metadata. Note that the metadata can be of ANY length; it only looks at the
+	 * last byte.
+	 *
 	 * @param metadata
 	 * @return the color
 	 */
 	public static ItemColor byMetadata(int metadata) {
 		int col = metadata & 0xF;
-		for(ItemColor iColor : ItemColor.values()) {
-			if(iColor.getMetadata() == col)
+		for (ItemColor iColor : ItemColor.values()) {
+			if (iColor.getMetadata() == col) {
 				return iColor;
+			}
 		}
 
-		MHFCMain.logger.log(Level.ERROR, "[%s] ERROR: Unable to find color for metadata %x! Defaulting to WHITE.", MHFCReference.main_modid, metadata);
+		MHFCMain.logger.error("Unable to find color for metadata %x! Defaulting to WHITE.", metadata);
 		return ItemColor.WHITE;
 	}
 
 	/**
-	 * Returns a float array of the color.
-	 * Index 0: R
-	 * Index 1: G
-	 * Index 2: B
-	 * @return
+	 *
+	 * @return the {@link Color} of this color.
 	 */
-	public float[] getFloats() {
-		float[] result = new float[3];
+	public Color getColor() {
 		int color = this.getRGB();
-		result[0] = ((float)(color >> 16 & 0xFF)/255.0F);
-		result[1] = ((float)(color >> 8  & 0xFF)/255.0F);
-		result[2] = ((float)(color       & 0xFF)/255.0F);
-
-
-		return result;
+		return Color.rgb(color >> 16 & 0xFF, color >> 8 & 0xFF, color & 0xFF);
 	}
 }
