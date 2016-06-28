c9c2ef86bdea2cd568c24e35d8e4e0efa6492df4
WorldSEnder <worldsbegin@gmx.de>
2016-06-28 15:05:27 +0200

    Added attack damage

--- src/main/java/mhfc/net/common/weapon/melee/ItemWeaponMelee.java
+++ src/main/java/mhfc/net/common/weapon/melee/ItemWeaponMelee.java
@@ -1,6 +1,10 @@
 package mhfc.net.common.weapon.melee;
 
+import com.google.common.collect.Multimap;
+
 import mhfc.net.common.weapon.ItemWeapon;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 
@@ -14,5 +18,14 @@ public abstract class ItemWeaponMelee<W extends MeleeWeaponStats> extends ItemWe
 	@Override
 	public EnumAction getItemUseAction(ItemStack stack) {
 		return EnumAction.block;
+	}
+
+	@Override
+	public Multimap<String, AttributeModifier> getAttributeModifiers(ItemStack stack) {
+		Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(stack);
+		multimap.put(
+				SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(),
+				new AttributeModifier(field_111210_e, "Weapon Attack", stats.getAttack(), 0));
+		return multimap;
 	}
 }
