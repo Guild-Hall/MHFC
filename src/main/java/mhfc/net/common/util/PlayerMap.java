0297beddca1ccf404f316d5f14ec0099cd4f108b
WorldSEnder <worldsbegin@gmx.de>
2016-07-14 04:58:00 +0200

    PlayerMap + PlayerSet for per-player collections

---
+++ src/main/java/mhfc/net/common/util/PlayerMap.java
@@ -0,0 +1,64 @@
+package mhfc.net.common.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.mojang.authlib.GameProfile;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+public class PlayerMap<T> extends HashMap<UUID, T> {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 7089554763707019448L;
+
+	public static UUID playerToKey(EntityPlayer player) {
+		if (player == null) {
+			return null;
+		}
+		return profileToKey(player.getGameProfile());
+	}
+
+	public static UUID profileToKey(GameProfile profile) {
+		if (profile == null) {
+			return null;
+		}
+		return profile.getId();
+	}
+
+	public PlayerMap() {
+		super();
+	}
+
+	public boolean containsPlayer(EntityPlayer key) {
+		return containsKey(playerToKey(key));
+	}
+
+	public boolean containsProfile(GameProfile key) {
+		return containsKey(profileToKey(key));
+	}
+
+	public T getPlayer(EntityPlayer key) {
+		return get(playerToKey(key));
+	}
+
+	public T getProfile(GameProfile key) {
+		return get(profileToKey(key));
+	}
+
+	public T putPlayer(EntityPlayer key, T value) {
+		return put(playerToKey(key), value);
+	}
+
+	public T putProfile(GameProfile key, T value) {
+		return put(profileToKey(key), value);
+	}
+
+	public void putAllPlayers(Map<? extends EntityPlayer, ? extends T> m) {
+		for (Map.Entry<? extends EntityPlayer, ? extends T> e : m.entrySet()) {
+			putPlayer(e.getKey(), e.getValue());
+		}
+	}
+}
---
+++ src/main/java/mhfc/net/common/util/PlayerSet.java
@@ -0,0 +1,15 @@
+package mhfc.net.common.util;
+
+import java.util.HashSet;
+import java.util.UUID;
+
+public class PlayerSet extends HashSet<UUID> {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 2061439866575218352L;
+
+	public PlayerSet() {
+		super();
+	}
+}
