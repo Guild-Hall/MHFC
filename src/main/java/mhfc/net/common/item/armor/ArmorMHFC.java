c894576dbd514578eebfd421fc816ccaaa3ae969
heltrato <jayrol_rante2006@yahoo.com>
2016-07-24 22:41:27 +0800

    Armors have now specific armor hearts display.

--- src/main/java/mhfc/net/common/item/armor/ArmorMHFC.java
+++ src/main/java/mhfc/net/common/item/armor/ArmorMHFC.java
@@ -1,6 +1,7 @@
 package mhfc.net.common.item.armor;
 
 import mhfc.net.MHFCMain;
+import mhfc.net.common.entity.type.EntityMHFCBase;
 import mhfc.net.common.item.ItemRarity;
 import mhfc.net.common.util.lib.MHFCReference;
 import net.minecraft.entity.EntityLivingBase;
@@ -31,18 +32,42 @@ public class ArmorMHFC extends ItemArmor implements ISpecialArmor {
 	}
 
 	@Override
-	public ArmorProperties getProperties(
-			EntityLivingBase player,
-			ItemStack armor,
-			DamageSource source,
-			double damage,
-			int slot) {
+	public ArmorProperties getProperties(EntityLivingBase player,ItemStack armor,DamageSource source,double damage, int slot) {
+		double armourReduction = 0.0;
+		double damageAmount = 0.25;
+		
+		if(slot == 0){
+			damageAmount = 3d / 20d * 0.6;
+		}else if(slot == 1){
+			damageAmount = 0.8;
+		}else if(slot == 2){
+			damageAmount = 6d / 20d * 0.6;
+		}else if(slot == 3){
+			damageAmount = 6d / 20d * 0.6;
+		}
+		 if (source.equals(DamageSource.causeMobDamage(player.getLastAttacker())))
+	        {	
+			 if(player.getLastAttacker() instanceof EntityMHFCBase){
+				 damageAmount = 2D/20 * 0.3;
+			 }
+			 
+	        }
+		
 		return null;
 	}
+	
+	public int chestArmordisplay;
+	public int LegArmordisplay;
+	public int headArmordisplay;
 
 	@Override
 	public int getArmorDisplay(EntityPlayer player, ItemStack armor, int slot) {
-		return armorHeart;
+		if(slot == 1){
+			return chestArmordisplay;
+		}else if (slot == 2) {
+			return LegArmordisplay;
+		}
+		return headArmordisplay;
 	}
 
 	@Override
@@ -50,10 +75,5 @@ public class ArmorMHFC extends ItemArmor implements ISpecialArmor {
 
 	}
 
-	public void displayInstancesStack(EntityLivingBase entity, int stack, int entID, DamageSource source) {
-		//WIP for armors.. display in the description how much the armor can absords lethal damage from mobs..
-		// SPECIFIC DAMAGE CALCULATIONS NOT JUST DIAMOND BASIS.
-		// still thinking of how to implement.. /Heltrato
-	}
 
 }
