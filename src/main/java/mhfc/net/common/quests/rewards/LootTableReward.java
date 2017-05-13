d4d97528cda1f397c965de2af71fc1cee6794b5f
WorldSEnder <worldsbegin@gmx.de>
2017-05-13 16:21:14 +0200

    Actually dropping stuff for the player

--- src/main/java/mhfc/net/common/quests/rewards/LootTableReward.java
+++ src/main/java/mhfc/net/common/quests/rewards/LootTableReward.java
@@ -1,7 +1,11 @@
 package mhfc.net.common.quests.rewards;
 
+import java.util.List;
+
 import mhfc.net.common.quests.api.IQuestReward;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.storage.loot.LootContext;
@@ -23,7 +27,24 @@ public class LootTableReward implements IQuestReward {
 				.worldServerForDimension(overworldDimension);
 		LootTable lootTable = world.getLootTableManager().getLootTableFromLocation(loottableLoc);
 		LootContext lootContext = new LootContext.Builder(world).withPlayer(player).withLuck(player.getLuck()).build();
-		lootTable.generateLootForPools(player.getRNG(), lootContext);
+		List<ItemStack> rewards = lootTable.generateLootForPools(player.getRNG(), lootContext);
+
+		boolean anyItemInInventory = false;
+		for (ItemStack reward : rewards) {
+			boolean wasAdded = player.inventory.addItemStackToInventory(reward);
+			anyItemInInventory |= wasAdded;
+			if (!wasAdded || !reward.isEmpty()) {
+				EntityItem droppedItem = player.dropItem(reward, false);
+				if (droppedItem != null) {
+					droppedItem.setNoPickupDelay();
+					droppedItem.setOwner(player.getName());
+				}
+			}
+		}
+		if (anyItemInInventory) {
+			// TODO: maybe also play a little sound...
+			player.inventoryContainer.detectAndSendChanges();
+		}
 	}
 
 	public ResourceLocation getLootTableLocation() {
