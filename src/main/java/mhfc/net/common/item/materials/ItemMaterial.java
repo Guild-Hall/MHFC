42f16b38151e234af2ace964fc6dfa45075b6e61
heltrato <jayrol_rante2006@yahoo.com>
2017-05-15 23:02:44 +0800

    Added the ItemMaterial ( sources of all materials )

---
+++ src/main/java/mhfc/net/common/item/materials/ItemMaterial.java
@@ -0,0 +1,14 @@
+package mhfc.net.common.item.materials;
+
+public class ItemMaterial {
+
+	/**
+	 * 
+	 * I will dispose all materials from every monster, gathering and etc. HERE
+	 * 
+	 * Heltrato.
+	 * 
+	 * 
+	 */
+
+}
