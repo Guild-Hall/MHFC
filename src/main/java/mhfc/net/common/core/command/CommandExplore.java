699f290d4d959bb8b2890fba2dd359ddfcdf3b32
WorldSEnder <worldsbegin@gmx.de>
2017-06-06 00:31:05 +0200

    Command explore prints message when it fails

--- src/main/java/mhfc/net/common/core/command/CommandExplore.java
+++ src/main/java/mhfc/net/common/core/command/CommandExplore.java
@@ -7,11 +7,14 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 
+import mhfc.net.MHFCMain;
 import mhfc.net.common.core.registry.MHFCExplorationRegistry;
 import mhfc.net.common.quests.world.QuestFlair;
 import mhfc.net.common.world.area.AreaRegistry;
+import mhfc.net.common.world.area.IActiveArea;
 import mhfc.net.common.world.area.IAreaType;
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
@@ -31,24 +34,44 @@ public class CommandExplore extends CommandBase {
 	}
 
 	@Override
-	public void execute(MinecraftServer server, ICommandSender sender, String[] arguments) {
+	public void execute(MinecraftServer server, ICommandSender sender, String[] arguments) throws CommandException {
 		if (!this.checkPermission(server, sender)) {
 			return;
 		}
+
+		if (!(sender instanceof EntityPlayerMP)) {
+			throw new CommandException("Command only useable by players");
+		}
+
 		EntityPlayerMP player = (EntityPlayerMP) sender;
 		if (arguments.length == 0) {
 			MHFCExplorationRegistry.releasePlayer(player).respawn();
 			return;
-		} else if (arguments.length == 1) {
+		}
+
+		if (arguments.length == 1) {
 			String targetAreaName = arguments[0];
 			IAreaType areaType = AreaRegistry.instance.getType(targetAreaName);
 			if (areaType == null) {
-				sender.sendMessage(new TextComponentString("Warning: the area type you choose did not exist"));
-			} else {
-				MHFCExplorationRegistry.transferPlayer(player, areaType, QuestFlair.DAYTIME);
+				sender.sendMessage(new TextComponentString("The area type " + targetAreaName + " does not exist"));
+				return;
 			}
+
+			MHFCExplorationRegistry.transferPlayer(player, areaType, QuestFlair.DAYTIME)
+					.whenComplete((a, e) -> onTransferComplete(player, a, e));
+			return;
+		}
+
+		sender.sendMessage(new TextComponentString("Too many arguments for command mhfcexplore"));
+	}
+
+	private void onTransferComplete(EntityPlayerMP player, IActiveArea area, Throwable exception) {
+		if (exception != null) {
+			TextComponentString failMessage = new TextComponentString("Failed to allocate an area for the player: " + exception.getMessage());
+			player.sendMessage(failMessage);
+			MHFCMain.logger().catching(exception);
 		} else {
-			sender.sendMessage(new TextComponentString("Too many arguments for command mhfcexplore"));
+			;
 		}
 	}
 
