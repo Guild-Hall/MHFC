1245ce73accceb2ef4ca1e635ef9ea1e6f6cc42d
WorldSEnder <worldsbegin@gmx.de>
2017-07-01 01:49:19 +0200

    Removed unnecessary ClientOnly

--- src/main/java/mhfc/net/common/util/stringview/Viewable.java
+++ src/main/java/mhfc/net/common/util/stringview/Viewable.java
@@ -1,9 +1,5 @@
 package mhfc.net.common.util.stringview;
 
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-@SideOnly(Side.CLIENT)
 public interface Viewable {
 	void appendTo(StringBuilder builder);
 
