f2462cd96b4ef6566e90525399ff0e25a2ddf9c1
WorldSEnder <worldsbegin@gmx.de>
2017-07-08 15:22:14 +0200

    Fixed crashing when starting a quest

--- src/main/java/mhfc/net/client/quests/handler/QuestClientInitHandler.java
+++ src/main/java/mhfc/net/client/quests/handler/QuestClientInitHandler.java
@@ -1,16 +1,15 @@
 package mhfc.net.client.quests.handler;
 
 import mhfc.net.client.quests.MHFCRegQuestVisual;
+import mhfc.net.common.network.handler.ThreadSafeMessageHandler;
 import mhfc.net.common.network.message.quest.MessageQuestInit;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-public class QuestClientInitHandler implements IMessageHandler<MessageQuestInit, IMessage> {
+public class QuestClientInitHandler extends ThreadSafeMessageHandler<MessageQuestInit, IMessage> {
 	@Override
-	public IMessage onMessage(MessageQuestInit message, MessageContext ctx) {
+	protected void handleLater(MessageQuestInit message, MessageContext ctx) {
 		MHFCRegQuestVisual.getService().onInitializationMessage(message);
 		MHFCRegQuestVisual.getService().logStats();
-		return null;
 	}
 }
--- src/main/java/mhfc/net/client/quests/handler/QuestScreenVisualHandler.java
+++ src/main/java/mhfc/net/client/quests/handler/QuestScreenVisualHandler.java
@@ -1,15 +1,22 @@
 package mhfc.net.client.quests.handler;
 
+import mhfc.net.MHFCMain;
 import mhfc.net.client.quests.MHFCRegQuestVisual;
+import mhfc.net.client.quests.api.IMissionInformation;
+import mhfc.net.common.network.handler.ThreadSafeMessageHandler;
 import mhfc.net.common.network.message.quest.MessageMissionUpdate;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-public class QuestScreenVisualHandler implements IMessageHandler<MessageMissionUpdate, IMessage> {
+public class QuestScreenVisualHandler extends ThreadSafeMessageHandler<MessageMissionUpdate, IMessage> {
 	@Override
-	public IMessage onMessage(MessageMissionUpdate message, MessageContext ctx) {
-		MHFCRegQuestVisual.getMissionInformation(message.getMissionId()).updateProperties(message.getUpdateData());
-		return null;
+	protected void handleLater(MessageMissionUpdate message, MessageContext ctx) {
+		String missionID = message.getMissionId();
+		IMissionInformation missionInformation = MHFCRegQuestVisual.getMissionInformation(missionID);
+		if (missionInformation == null) {
+			MHFCMain.logger().error("Receiving update message for invalid mission {}, ignoring", missionID);
+		} else {
+			missionInformation.updateProperties(message.getUpdateData());
+		}
 	}
 }
